2ea2a0ce3cf5b25dfa176d956b239e01de1d5590
JonasAerts <jonas.aerts@student.uhasselt.be>
2020-12-21 16:58:15 +0100

    Made Player charackter

---
+++ projectGame/src/Character/SharedPlayer.c
@@ -0,0 +1,21 @@
+
+//{{BLOCK(SharedPlayer)
+
+//======================================================================
+//
+//	SharedPlayer, 16x16@8, 
+//	+ palette 5 entries, not compressed
+//	Total size: 10 = 10
+//
+//	Time-stamp: 2020-12-21, 15:07:25
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+const unsigned short SharedPlayerPal[6] __attribute__((aligned(4)))=
+{
+	0x0000,0x3F1E,0x0C0E,0x14A5,0x0500,
+};
+
+//}}BLOCK(SharedPlayer)
