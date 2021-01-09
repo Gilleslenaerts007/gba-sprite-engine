ffc80691fa9ff466a057c6a64ad918483fa37f63
Gilleslenaerts007 <Gilleslenaerts007@gmail.com>
2021-01-09 20:53:23 +0100

    Revert "Revert "Merge branch 'Jonas'""
    This reverts commit 3868b6bbd6e338cc48e97fb716570ecd03361fc0.

--- projectGame/src/scene_Chapter1.cpp
+++ projectGame/src/scene_Chapter1.cpp
@@ -54,7 +54,6 @@ std::vector<Sprite *> scene_Chapter1::sprites() {
  * Other entities/enemies/boss are defined per scene class.
  */
 void scene_Chapter1::load() {
-
     /*
      * Bij de palletten moeten de kleuren in 16bit formaat opgemaakt worden.
      * De tiles en map mogen 32bit opgemaakt/omgezet worden.
@@ -95,12 +94,12 @@ void scene_Chapter1::load() {
     bulletVerti = builder
             .withData(BulletVertiTiles, sizeof(BulletVertiTiles))
             .withSize(SIZE_16_16)
-            .withLocation(1, 1)
+            .withLocation(112, 72)
             .buildPtr();
     bulletHori = builder
             .withData(BulletHoriTiles, sizeof(BulletHoriTiles))
             .withSize(SIZE_16_16)
-            .withLocation(1, 1)
+            .withLocation(112, 72)
             .buildPtr();
 
 
@@ -145,32 +144,39 @@ void scene_Chapter1::tick(u16 keys) {
         playerPosX = player->getX();
         playerPosY = player->getY();
 
-        if (moveTimerPlayer >= 7)
+        if (movetimer >= 7)
         {
             moveflag = !moveflag;
-            moveTimerPlayer = 0;
+            movetimer = 0;
         }
 
         switch(keys)
         {
             case KEY_LEFT:  if(moveflag)player->animateToFrame(7);
                             else player->animateToFrame(8);
-                            staticPlayerModel = 8;
+                            staticPlayerModel = 7;
                             boolPlayerFlipHori = true;
+                            boolPlayerShootHori = true;
+                            boolPlayerShootVerti = false;
                             if (scrollX > 0 && playerPosX <= 112) { scrollX -= 1; player->setVelocity(0,0);}
                             else player->setVelocity(-1, 0);
                             break;
 
             case KEY_RIGHT: if(moveflag)player->animateToFrame(7);
                             else player->animateToFrame(8);
-                            staticPlayerModel = 7;
                             boolPlayerFlipHori = false;
+                            boolPlayerShootHori = true;
+                            boolPlayerShootVerti = false;
+                            staticPlayerModel = 7;
                             if (scrollX < 260 && playerPosX >= 112) { scrollX += 1; player->setVelocity(0,0);}
                             else player->setVelocity(+1, 0);
                             break;
 
             case KEY_DOWN:  if(moveflag)player->animateToFrame(3);
                             else player->animateToFrame(2);
+                            boolPlayerFlipHori = false;
+                            boolPlayerShootHori = false;
+                            boolPlayerShootVerti = true;
                             staticPlayerModel = 1;
                             if (scrollY < 340 && playerPosY >= 72) { scrollY += 1; player->setVelocity(0,0);}
                             else  player->setVelocity(0, +1);
@@ -178,216 +184,104 @@ void scene_Chapter1::tick(u16 keys) {
 
             case KEY_UP:    if(moveflag)player->animateToFrame(5);
                             else player->animateToFrame(6);
+                            boolPlayerFlipHori = false;
+                            boolPlayerShootHori = false;
+                            boolPlayerShootVerti = true;
                             staticPlayerModel = 4;
                             if (scrollY > 0 && playerPosY <= 72) { scrollY -= 1; player->setVelocity(0,0);}
                             else player->setVelocity(0, -1);
                             break;
 
+            /*case KEY_UP:    if(moveflag)player->animateToFrame(5);
+                            else player->animateToFrame(6);
+                            player->flipHorizontally(false);
+                            staticPlayerModel = 4;
+                            if (scrollY > 0 && playerPosY <= 72) { scrollY -= 1; player->setVelocity(0,0);}
+                            else player->setVelocity(0, -1);
+                            break;
+                            */
+
         }
-        boolPlayerMoving = true;
-        moveTimerPlayer++;
-        moveTimerEnemy++;
+        player->flipHorizontally(boolPlayerFlipHori);
+        movetimer++;
+        moving = true;
     }
     else
     {
         player->setVelocity(0, 0);
         rotation = 0;
         player->animateToFrame(staticPlayerModel);
-        boolPlayerMoving = false;
+        moving = false;
+        boolPlayerShootHori = false;
+        boolPlayerShootVerti = false;
     }
-    player->flipHorizontally(boolPlayerFlipHori);
 
     //rotation += rotationDiff;
     //enemy.get()->rotate(rotation);
     //player.get()->rotate(rotation);
-    //
-    //
+    bg_C1.get()->scroll(scrollX, scrollY);
+
 
     UpdateGame();
 
-    bg_C1.get()->scroll(scrollX, scrollY);
+
 
 };
 
 void scene_Chapter1::UpdateGame() {
 
+    if (boolPlayerShootHori || boolPlayerShootVerti)
+    {
+        bulletHori->moveTo(playerPosX, playerPosY);
+        bulletHori->makeAnimated(0,8,3);
+        bulletVerti->moveTo(playerPosX, playerPosY);
+        bulletVerti->makeAnimated(0,8,3);
+
+        if (boolPlayerShootVerti){
+            if (staticPlayerModel == 1){
+                bulletVerti->flipVertically(true);
+                bulletVerti->setVelocity(0,4);
+            }
+            else {
+                bulletVerti->flipVertically(false);
+                bulletVerti->setVelocity(0,-4);
+            }
+        }
+        else{
+            if (boolPlayerFlipHori){
+                bulletHori->flipHorizontally(true);
+                bulletHori->setVelocity(-4,0);
+            }
+            else {
+                bulletHori->flipHorizontally(false);
+                bulletHori->setVelocity(4,0);
+            }
+        }
+        //updateSprites = true;
+        boolPlayerShootHori = false;
+        boolPlayerShootVerti = false;
+    }
+
     if ( currentEnemies <= totalEnemies)
     {
-        if (boolPlayerMoving && (spawnerTime >= 20) ) //         if (boolPlayerMoving && spawnerTime <= 5000) //
+        if (moving && spawnerTime <= 5000)
         {
             enemies.push_back( builder
                                        .withData(EnemyFullTiles, sizeof(EnemyFullTiles))
                                        .withSize(SIZE_16_16)
                                                //.withVelocity(1, 1)
-                                       .withLocation(rand()%GBA_SCREEN_WIDTH +1, rand()%GBA_SCREEN_HEIGHT+1)
+                                       .withLocation(rand() % player->getX(), rand() % player->getY())
                                        .withVelocity(rand() % 1, rand() % 1)
                                        .buildPtr());
             spawnerTime = 0;
             currentEnemies++;
-            engine->updateSpritesInScene();
+            updateSprites = true;
         }
-        else spawnerTime++;
+        spawnerTime++;
     }
 
-    // Kunnen hier zeggen dat we bv een movetimer voor enemy hebben zodat de speler 2x rapper is en dus 1 zet voor is.
-    if (!enemies.empty() )// && moveTimerEnemy >= 2)
-    {
-        UpdateMovements();
-    }
-
-    /*
     if (updateSprites) {
         engine->updateSpritesInScene();
         updateSprites = false;
     }
-     */
 }
-
-void scene_Chapter1::UpdateMovements(){
-
-    //loopEnemies = 0;
-
-    enemyPosX = enemies[loopEnemies]->getX();
-    enemyPosY = enemies[loopEnemies]->getY();
-    if (moveTimerEnemy >= 2)
-    {
-        moveflagEnemy = !moveflagEnemy;
-        moveTimerEnemy = 0;
-    }
-
-    if ( (enemyPosX == playerPosX) || (enemyPosY == playerPosY) )
-    {
-        //shoot();
-        enemies[loopEnemies]->animateToFrame(staticEnemyModel);
-    }
-    else
-    {
-        trackingY = abs (enemyPosY - playerPosY);
-        trackingX = abs (enemyPosX - playerPosX);
-
-        if (trackingX < trackingY)
-        {
-            if (enemyPosX > playerPosX)
-            {
-                if(moveflagEnemy)enemies[loopEnemies]->animateToFrame(7);
-                else enemies[loopEnemies]->animateToFrame(8);
-                staticEnemyModel = 7;
-                enemies[loopEnemies]->flipHorizontally(true);
-                enemyPosX--;
-            }
-            else
-            {
-                if(moveflagEnemy)enemies[loopEnemies]->animateToFrame(7);
-                else enemies[loopEnemies]->animateToFrame(8);
-                staticEnemyModel = 8;
-                enemies[loopEnemies]->flipHorizontally(false);
-                enemyPosX++;
-            }
-        }
-        else if (trackingY < trackingX)
-        {
-            if (enemyPosY < playerPosY)
-            {
-                if(moveflagEnemy)enemies[loopEnemies]->animateToFrame(3);
-                else enemies[loopEnemies]->animateToFrame(2);
-                staticEnemyModel = 1;
-                enemyPosY++;
-            }
-            else
-            {
-                if(moveflagEnemy)enemies[loopEnemies]->animateToFrame(5);
-                else enemies[loopEnemies]->animateToFrame(6);
-                staticEnemyModel = 4;
-                enemyPosY--;
-            }
-        }
-       moveTimerEnemy++;
-    }
-
-    if (oldScrollX == scrollX){
-        enemies[loopEnemies]->moveTo(enemyPosX, enemyPosY);
-    }
-    if (oldScrollX > scrollX){
-        enemies[loopEnemies]->moveTo(enemyPosX+enemyMoveSpeed, enemyPosY);
-    }
-    else if (oldScrollX < scrollX){
-        enemies[loopEnemies]->moveTo(enemyPosX-enemyMoveSpeed, enemyPosY);
-    }
-    if (oldScrollY > scrollY){
-        enemies[loopEnemies]->moveTo(enemyPosX, enemyPosY+enemyMoveSpeed);
-    }
-    else if (oldScrollY < scrollY){
-        enemies[loopEnemies]->moveTo(enemyPosX, enemyPosY-enemyMoveSpeed);
-    }
-
-    if (loopEnemies < enemies.size() -1 )
-    {
-        loopEnemies++;
-    }
-    else loopEnemies = 0;
-
-    oldScrollX = scrollX;
-    oldScrollY = scrollY;
-}
-
-/* bkup code
- *                 if (enemyPosX > playerPosX || enemyPosX < playerPosX)
-    enemyPosX = enemies[loopEnemies]->getX();
-    enemyPosY = enemies[loopEnemies]->getY();
-    if (moveTimerEnemy >= 2)
-    {
-        moveflagEnemy = !moveflagEnemy;
-        moveTimerEnemy = 0;
-    }
-
-    if ( (enemyPosX == playerPosX) || (enemyPosY == playerPosY) )
-    {
-        //shoot();
-        enemies[loopEnemies]->animateToFrame(staticEnemyModel);
-    }
-    else
-    {
-        trackingY = abs (enemyPosY - playerPosY);
-        trackingX = abs (enemyPosX - playerPosX);
-
-        if (trackingX < trackingY)
-        {
-            if (enemyPosX > playerPosX)
-            {
-                if(moveflagEnemy)enemies[loopEnemies]->animateToFrame(7);
-                else enemies[loopEnemies]->animateToFrame(8);
-                staticEnemyModel = 7;
-                enemies[loopEnemies]->flipHorizontally(true);
-                enemies[loopEnemies]->moveTo(enemyPosX-1, enemyPosY);
-            }
-            else
-            {
-                if(moveflagEnemy)enemies[loopEnemies]->animateToFrame(7);
-                else enemies[loopEnemies]->animateToFrame(8);
-                staticEnemyModel = 8;
-                enemies[loopEnemies]->flipHorizontally(false);
-                enemies[loopEnemies]->moveTo(enemyPosX+1, enemyPosY);
-            }
-        }
-        else if (trackingY < trackingX)
-        {
-            if (enemyPosY < playerPosY)
-            {
-                if(moveflagEnemy)enemies[loopEnemies]->animateToFrame(3);
-                else enemies[loopEnemies]->animateToFrame(2);
-                staticEnemyModel = 1;
-                enemies[loopEnemies]->moveTo(enemyPosX, enemyPosY+1);
-            }
-            else
-            {
-                if(moveflagEnemy)enemies[loopEnemies]->animateToFrame(5);
-                else enemies[loopEnemies]->animateToFrame(6);
-                staticEnemyModel = 4;
-                enemies[loopEnemies]->moveTo(enemyPosX, enemyPosY-1);
-            }
-        }
-       moveTimerEnemy++;
-    }
-
-
- */
